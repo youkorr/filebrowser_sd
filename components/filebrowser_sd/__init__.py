0ef658ab2c8b200914e201900247945bd2798121
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-21 22:21:46 +0100

    Update __init__.py

--- components/filebrowser_sd/__init__.py
+++ components/filebrowser_sd/__init__.py
@@ -6,9 +6,17 @@ DEPENDENCIES = ['sd_mmc_card']
 AUTO_LOAD = ['sd_mmc_card']
 
 CONF_MOUNT_POINT = 'mount_point'
+CONF_SMB_SHARES = 'smb_shares'
 
 filebrowser_sd_ns = cg.esphome_ns.namespace('filebrowser_sd')
 FileBrowserSDComponent = filebrowser_sd_ns.class_('FileBrowserSDComponent', cg.Component)
+
+def validate_shares(value):
+    if isinstance(value, str):
+        return [value]
+    if isinstance(value, list):
+        return value
+    raise cv.Invalid("smb_shares must be a string or list of strings")
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(FileBrowserSDComponent),
@@ -16,6 +24,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_USERNAME): cv.string,
     cv.Required(CONF_PASSWORD): cv.string,
     cv.Optional(CONF_MOUNT_POINT, default='/sdcard'): cv.string,
+    cv.Optional(CONF_SMB_SHARES): validate_shares,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -26,3 +35,7 @@ async def to_code(config):
     cg.add(var.set_username(config[CONF_USERNAME]))
     cg.add(var.set_password(config[CONF_PASSWORD]))
     cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
+    
+    if CONF_SMB_SHARES in config:
+        cg.add(var.set_smb_shares(config[CONF_SMB_SHARES]))
+
