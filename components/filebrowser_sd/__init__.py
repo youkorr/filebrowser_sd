49a375fb16385dc1e3987d802e8e4f9a81c81a6b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-21 18:40:52 +0100

    Update __init__.py

--- components/filebrowser_sd/__init__.py
+++ components/filebrowser_sd/__init__.py
@@ -9,18 +9,22 @@ filebrowser_sd_ns = cg.esphome_ns.namespace('filebrowser_sd')
 FileBrowserSDComponent = filebrowser_sd_ns.class_('FileBrowserSDComponent', cg.Component)
 
 # Constantes pour la configuration
-CONF_BASE_PATH = 'base_path'
 CONF_MOUNT_POINT = 'mount_point'
-CONF_MAX_FILES = 'max_files'
-CONF_FORMAT_IF_MOUNT_FAILED = 'format_if_mount_failed'
+CONF_ADDRESS_IP = 'adress_ip'
+CONF_PORT = 'port'
+CONF_ROOT_PATH = 'root_path'
+CONF_USERNAME = 'username'
+CONF_PASSWORD = 'password'
 
 # Configuration du composant
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(FileBrowserSDComponent),
-    cv.Optional(CONF_BASE_PATH, default='/filebrowser'): cv.string,
-    cv.Optional(CONF_MOUNT_POINT, default='/sdcard'): cv.string,
-    cv.Optional(CONF_MAX_FILES, default=5): cv.int_range(min=1, max=20),
-    cv.Optional(CONF_FORMAT_IF_MOUNT_FAILED, default=False): cv.boolean,
+    cv.Required(CONF_MOUNT_POINT): cv.string,
+    cv.Optional(CONF_ADDRESS_IP): cv.string,
+    cv.Optional(CONF_PORT): cv.port,
+    cv.Optional(CONF_ROOT_PATH, default="/"): cv.string,
+    cv.Optional(CONF_USERNAME, default="admin"): cv.string,
+    cv.Optional(CONF_PASSWORD): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -28,7 +32,12 @@ async def to_code(config):
     await cg.register_component(var, config)
     
     # Configurer les options
-    cg.add(var.set_base_path(config[CONF_BASE_PATH]))
     cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
-    cg.add(var.set_max_files(config[CONF_MAX_FILES]))
-    cg.add(var.set_format_if_mount_failed(config[CONF_FORMAT_IF_MOUNT_FAILED]))
+    if CONF_ADDRESS_IP in config:
+        cg.add(var.set_address_ip(config[CONF_ADDRESS_IP]))
+    if CONF_PORT in config:
+        cg.add(var.set_port(config[CONF_PORT]))
+    cg.add(var.set_root_path(config[CONF_ROOT_PATH]))
+    cg.add(var.set_username(config[CONF_USERNAME]))
+    if CONF_PASSWORD in config:
+        cg.add(var.set_password(config[CONF_PASSWORD]))
