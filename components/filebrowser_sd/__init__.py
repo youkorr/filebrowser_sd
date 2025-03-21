f2d95cafb62b260fd1da5b72fa43802c9b654b2f
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-21 19:05:41 +0100

    Update __init__.py

--- components/filebrowser_sd/__init__.py
+++ components/filebrowser_sd/__init__.py
@@ -9,22 +9,18 @@ filebrowser_sd_ns = cg.esphome_ns.namespace('filebrowser_sd')
 FileBrowserSDComponent = filebrowser_sd_ns.class_('FileBrowserSDComponent', cg.Component)
 
 # Constantes pour la configuration
+CONF_BASE_PATH = 'base_path'
 CONF_MOUNT_POINT = 'mount_point'
-CONF_ADDRESS_IP = 'adress_ip'
-CONF_PORT = 'port'
-CONF_ROOT_PATH = 'root_path'
-CONF_USERNAME = 'username'
-CONF_PASSWORD = 'password'
+CONF_MAX_FILES = 'max_files'
+CONF_FORMAT_IF_MOUNT_FAILED = 'format_if_mount_failed'
 
 # Configuration du composant
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(FileBrowserSDComponent),
-    cv.Required(CONF_MOUNT_POINT): cv.string,
-    cv.Optional(CONF_ADDRESS_IP): cv.string,
-    cv.Optional(CONF_PORT): cv.port,
-    cv.Optional(CONF_ROOT_PATH, default="/"): cv.string,
-    cv.Optional(CONF_USERNAME, default="admin"): cv.string,
-    cv.Optional(CONF_PASSWORD): cv.string,
+    cv.Optional(CONF_BASE_PATH, default='/filebrowser'): cv.string,
+    cv.Optional(CONF_MOUNT_POINT, default='/sdcard'): cv.string,
+    cv.Optional(CONF_MAX_FILES, default=5): cv.int_range(min=1, max=20),
+    cv.Optional(CONF_FORMAT_IF_MOUNT_FAILED, default=False): cv.boolean,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -32,12 +28,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     
     # Configurer les options
+    cg.add(var.set_base_path(config[CONF_BASE_PATH]))
     cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
-    if CONF_ADDRESS_IP in config:
-        cg.add(var.set_address_ip(config[CONF_ADDRESS_IP]))
-    if CONF_PORT in config:
-        cg.add(var.set_port(config[CONF_PORT]))
-    cg.add(var.set_root_path(config[CONF_ROOT_PATH]))
-    cg.add(var.set_username(config[CONF_USERNAME]))
-    if CONF_PASSWORD in config:
-        cg.add(var.set_password(config[CONF_PASSWORD]))
+    cg.add(var.set_max_files(config[CONF_MAX_FILES]))
+    cg.add(var.set_format_if_mount_failed(config[CONF_FORMAT_IF_MOUNT_FAILED]))
